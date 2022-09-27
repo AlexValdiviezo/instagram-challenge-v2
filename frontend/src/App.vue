c6a2b75ed49ce2d17cfdcebe34a832d29311417c
Nazareno <lemongnz>
2022-09-27 17:10:14 -0300

    frontend added2

---
+++ frontend/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app"> 
+    <div v-for="(post, index) in posts" :key="index"> 
+      <p>{{post.username}}</p>
+      <img :src="`${post.image}`" />
+      <span>likes: {{post.likes}}</span>
+      <p>{{post.title}}</p>
+    </div>
+    <button @click="callBack">Hacer llamdo</button> 
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+// TODO: hacer el llamado cuando se renderiza el componente useEffect({}, [])
+
+export default {
+  
+  name: 'App',
+  components: {
+  },
+  data() {
+    return {
+      posts: null
+    }
+  },
+  methods: {
+    callBack() {
+      axios.get('http://localhost:4001/api/post')
+      .then((res) => {
+        this.posts = res.data.posts
+      })
+      .catch(error => console.log(error))
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
