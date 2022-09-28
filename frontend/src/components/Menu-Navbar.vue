439b430193296fb44d13b9e526f1d46240be2f29
AlexValdiviezo <alex.valdiviezo@sondeos.com.ar>
2022-09-28 17:29:32 -0300

    frontend actually

---
+++ frontend/src/components/Menu-Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app-bar fixed outlined color="grey darken-3 white--text">
+    <v-app-bar-nav-icon color="white"></v-app-bar-nav-icon>
+    <v-spacer/>
+    <v-app-bar-title>lnstagram</v-app-bar-title>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+    name:"Menu-Navbar"
+}
+</script>
+
+<style scoped>
+</style>
