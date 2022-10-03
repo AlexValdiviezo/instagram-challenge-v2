0317ca755a604d85eb34a4b8b946695b5b31977d
ramirosandoval <ramiro_sando@outlook.com>
2022-10-03 17:58:55 -0300

    NavBar component added

---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,39 @@
+<template>
+
+    <nav>
+        <v-toolbar>
+            <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+            
+            <v-spacer></v-spacer>
+            
+            <v-toolbar-title>Instagram Challenge</v-toolbar-title>
+        </v-toolbar>
+
+        <v-navigation-drawer 
+            v-model="drawer"
+            fixed
+            left
+            temporary
+            absolute
+            class="pt-4 pb-4"
+        >
+            <p>a</p>
+        </v-navigation-drawer>
+    </nav>
+
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      drawer: false,
+      group: null,
+    }),
+
+    watch: {
+      group () {
+        this.drawer = false
+      },
+    },
+  }
+</script>
