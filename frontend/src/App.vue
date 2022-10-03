98d234dd52b02d69c9f7110acd57b5bc5f6f8f7e
ramirosandoval <ramiro_sando@outlook.com>
2022-10-03 18:02:42 -0300

    toolbar added

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,23 +2,23 @@
   <v-app>
     <server-not-found-component v-if="serverNotFound"/>
     <alert-component v-if="alert"/>
-    <menu-navbar />
-    <menu-options/>
-    <card-container/>
+
+    <NavBar/>
+
+    <card-container></card-container>
   </v-app>
 </template>
 
 <script>
 import AlertComponent from './components/AlertComponent.vue';
 import CardContainer from './components/CardContainer.vue'
-import MenuNavbar from './components/MenuNavbar.vue'
-import MenuOptions from './components/MenuOptions.vue'
 import ServerNotFoundComponent from './components/ServerNotFoundComponent.vue';
+import NavBar from './components/NavBar.vue';
 
 
 export default {
   name: 'App',
-  components: { MenuNavbar, CardContainer, MenuOptions, AlertComponent, ServerNotFoundComponent},
+  components: { CardContainer, AlertComponent, ServerNotFoundComponent, NavBar },
   computed:{
     alert(){
       return this.$store.state.alertModule.alert
--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -17,7 +17,20 @@
             absolute
             class="pt-4 pb-4"
         >
-            <p>a</p>
+        <v-app-bar-title class="text-center">Actions</v-app-bar-title>
+        <v-divider></v-divider>
+        <v-list
+            nav
+            dense
+        >
+        <v-list-item-group
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-btn block color="rgba(0,0,0,0)" depressed>
+            <v-list-item-title class="text-center font-weight-medium text-h6">ADD POST</v-list-item-title>
+          </v-btn>
+        </v-list-item-group>
+      </v-list>
         </v-navigation-drawer>
     </nav>
 
